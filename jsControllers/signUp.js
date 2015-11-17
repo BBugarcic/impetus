4fa4c4d90f66b22e43adfb29047e423069071647
Branislav Bugarcic <bugarcic@gmail.com>
2015-11-17 20:26:02 +0100

    handling used usernames

--- jsControllers/signUp.js
+++ jsControllers/signUp.js
@@ -2,11 +2,17 @@
 $(document).ready(function(){
 
 		$('#subBtn1').click(function() {
+			var username = $('username').val();
+			var email = $('email').val();
 			var pass = $('#pass').val();
 			var conf = $('#conf').val();
-			if (pass !== conf) {
+			if (username == '' || email == '' || pass == '' || conf == '') {
+				alert("All fields are required");
+			}
+			else if (pass !== conf) {
 				alert("Password and confirmation are not the same.");
 			}
+			
 			else{
 				$.ajax({
 					type: "POST",
@@ -19,7 +25,11 @@ $(document).ready(function(){
 				dataType: 'json',
 				success: function (response) {
 					if(response.status == 1){
+						// if new user is registered
+						// redirect to...
 						$(window).attr('location','http://localhost/view/loggedIn.php');
+					} else {
+						alert("Username is already taken.")
 					}
 				},
 				error: function(xhr,ajaxOptions,thrownError) {
@@ -35,5 +45,6 @@ $(document).ready(function(){
 		
 			
 });
+
 
 
