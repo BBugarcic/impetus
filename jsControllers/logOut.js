346366786765a9275a20c2ae59bce3f92b7ce76a
Branislav Bugarcic <bugarcic@gmail.com>
2015-11-17 20:37:00 +0100

    removing testing alerts

---
+++ jsControllers/logOut.js
@@ -0,0 +1,29 @@
+
+// redirect user to home page
+// when clicked on log out button
+$(document).ready(function() {
+	$('#logOut').click(function() {
+		
+		$.ajax({
+			type: "POST",
+			url:'/phpControllers/logOut.php',
+			data: {
+				val: 'logOut'	
+			},
+			dataType: 'json',
+			success: function (response) {
+				if(response.status == 1) {
+					// redirect to...					
+					$(window).attr('location','http://localhost/index.php');
+				}
+			},
+			error: function(xhr,ajaxOptions,thrownError) {
+				alert(thrownError);
+			}
+		});
+		return false;
+	});
+});
+
+
+
