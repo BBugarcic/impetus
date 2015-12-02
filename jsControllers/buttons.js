0089d2a4f13423a8dd8a4c5501a85dfad1e04f0c
Branislav Bugarcic <bugarcic@gmail.com>
2015-12-02 12:32:49 +0100

    edit item, alerts when deleting items and current list

--- jsControllers/buttons.js
+++ jsControllers/buttons.js
@@ -1,4 +1,9 @@
 $(document).ready(function(event) {
+	
+	$('#myTables').DataTable( {
+    	select: true
+	} );
+
 	//disable add task button before creating list
 	$("#addTask").prop("disabled", true);
 	
@@ -18,8 +23,8 @@ $(document).ready(function(event) {
 		// send list title to php controller via ajax
 	 	var title = $("#NewListTitle").val();
 		if (title == '') {
-			//alert("List title is requiered");
-			$("#required").html("<div class='alert alert-danger alert-dismissible col-md-4' role='alert'>" +
+			// alert if list title is not provided;
+			$("#requiredAlert").html("<div class='alert alert-danger alert-dismissible col-md-4' role='alert'>" +
   						"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
   						"<strong>List title is required!</strong></div>");
 		} else {
@@ -37,7 +42,9 @@ $(document).ready(function(event) {
 						//enable add task button after creating list
 						$("#addTask").prop("disabled", false);
 					} else {
-						alert("Ups! Something went wrong, please reload page and try again.");
+						$("#requiredAlert").html("<div class='alert alert-danger alert-dismissible col-md-4' role='alert'>" +
+									"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+									"<strong>Ups! Something went wrong. Please reload page and try again.</strong></div>");
 					}
 				},
 				error: function(xhr,ajaxOptions,thrownError) {
@@ -52,37 +59,46 @@ $(document).ready(function(event) {
 		
 		
 		// delete current list
-		$("#deleteCurrList").click(function(){	
+		$("#deleteCurrList").click(function(){
+			$("#deleteAlert").html("<div class='alert alert-danger alert-dismissible col-md-10' role='alert'>" +
+							"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+							"<strong>Are you sure you want to delete the list?</strong>" +
+							"<button id='yes' type='button' class='btn btn-default'>Yes</button>" +
+							"<button id='no' type='button' class='btn btn-default' data-dismiss='alert'>No</button></div>");
+								
 			var currTitle = $("#currentTitle").text();	
-			$.ajax({
-				type: "POST",
-				url: "../phpControllers/delCurrList.php",
-				data: {
-					currTitle: currTitle,
-				},
-				dataType: "json",
-				success: function(response) {
-					if(response.status == 1) {
-						$("#titleRow").hide("slow");
-						$("#itemRow").empty();
-						$("#itemRow").hide("slow");
-						$("#addItemRow").hide("slow");
-						$("#itemCategory").hide("slow");
-						$("#addTask").prop("disabled", true);
-					} else {
-						alert("Sorry! Something went wrong. Open your list first, and than delete it.");
+			$("#yes").click(function(){
+				$.ajax({
+					type: "POST",
+					url: "../phpControllers/delCurrList.php",
+					data: {
+						currTitle: currTitle,
+					},
+					dataType: "json",
+					success: function(response) {
+						if(response.status == 1) {
+							$("#titleRow").hide("slow");
+							$("#deleteAlert").hide("slow");
+							$("#itemRow").empty();
+							$("#itemRow").hide("slow");
+							$("#addItemRow").hide("slow");
+							$("#itemCategory").hide("slow");
+							$("#addTask").prop("disabled", true);
+						} else {
+							$("#deleteAlert").html("<div class='alert alert-danger alert-dismissible col-md-10' role='alert'>" +
+										"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+										"<strong>Ups! Something went wrong. Please open your list in My Lists and delete it.</strong></div>");
+						}
+					},
+					error: function(xhr,ajaxOptions,thrownError) {
+						alert(thrownError);
 					}
-				},
-				error: function(xhr,ajaxOptions,thrownError) {
-					alert(thrownError);
-				}
+				});
 			});
 		});
 			event.preventDefault();		
 	 	});
 		
-
-	 
 	 // cancel creating list
 	 $("#listCancel").click(function(event) {
 		$("#newList").prop("disabled", false);
@@ -116,8 +132,7 @@ $(document).ready(function(event) {
 				type: "POST",
 				url: "../phpControllers/newItem.php",
 				data: {
-					item: inputToDo,
-					listTitle: $("#NewListTitle").val()
+					item: inputToDo
 				},
 				dataType: "json",
 				success: function(response) {
@@ -126,13 +141,14 @@ $(document).ready(function(event) {
 						"<div class='checkbox'><label>" +
 						"<input id='" + response.item_id + "' type='checkbox'>" +
 						"</label></div></div>" +
-						"<div id='ToDo' class='col-md-7'><p id='item'>" + inputToDo + "</p></div>" +
+						"<div id='" + response.item_id + "' class='col-md-7 edit'>" + inputToDo + "</div>" +
+						"<div class='edit_area' id='div_2'></div>" +
 						"<div id='edit' class='col-md-1'>" +
 						"<button id='" + response.item_id + "' type='submit' class='btn btn-default btn-xm'>" +
 						"<span class='glyphicon glyphicon-wrench' aria-hidden='true'></span></button></div>" +
 						"<div id='remove' class='col-md-1'>" +
 						"<button id='" + response.item_id + "' type='button' class='btn btn-default btn-xm'>" +
-						"<span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button></div></div>");
+						"<span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button></div></div>").find('.edit').editable('../phpControllers/edit.php');
 						$("#newItem").show("slow");
 					} else {
 						alert("Sorry! Something went wrong.");
@@ -148,11 +164,11 @@ $(document).ready(function(event) {
 
 	 // remove and edit button handler
 	$( "#itemRow" ).on( "click", "button", function() {
-		alert('cao');
 		var elementId = $(this).attr('id');
 		var parentId = $(this).closest("div").attr("id");
 		alert("perent div: " + parentId);		
 		alert(elementId);
+		
 		// delete item
 		// send json array to php controller
 		// remove div after success
@@ -162,7 +178,7 @@ $(document).ready(function(event) {
 				type: "POST",
 				url: "../phpControllers/deleteItem.php",
 				data: {
-					item_id: $(this).attr('id'),
+					item_id: $(this).attr('id')
 				},
 				dataType: "json",
 				success: function(response) {
@@ -179,13 +195,31 @@ $(document).ready(function(event) {
 				}
 			});	
 		}
-
 	});
 
+	// check item when done
+	// uncheck item if done
 	$( "#itemRow" ).on( "click", "input", function() {
-		alert('cao');
 		var elementId = $(this).attr('id');
+		$(".edit").toggleClass("strike");
 		alert(elementId);
-	});	
-			
+		$.ajax({
+			type: "POST",
+			url: "../phpControllers/done.php",
+			data: {
+				item_id: $(this).attr('id'),
+			},
+			dataType: "json",
+			success: function(response) {
+				if(response.status == 1){
+					alert("uradjeno");
+				} else if (response.status == 0) {
+					alert("nije uradjeno");	
+				}
+				else {
+					alert("Sorry! Something went wrong.");
+				}
+			}
+		});
+	});				
 });
