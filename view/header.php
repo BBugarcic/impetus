87ea52f7dd8845369f86cd96b52eb2a28036940f
Branislav Bugarcic <bugarcic@gmail.com>
2015-12-11 12:44:30 +0100

    added checkbox plugin

--- view/header.php
+++ view/header.php
@@ -5,8 +5,12 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
+		<!-- bootstrap -->
 		<link href="/includes/css/bootstrap.css" rel="stylesheet">
 		<link href="/includes/css/styles.css" rel="stylesheet">
+		
+		<!-- checkbox plugin http://montrezorro.github.io/bootstrap-checkbox/ -->
+		<link href="/includes/css/bootstrap-checkbox.css" rel="stylesheet">
 	
 		
 	</head>
