38b284c09125823c4f90aef81b49aea49b526ce3
Branislav Bugarcic <bugarcic@gmail.com>
2015-12-11 12:27:59 +0100

    handling possible input errors

--- jsControllers/signUp.js
+++ jsControllers/signUp.js
@@ -2,19 +2,14 @@
 $(document).ready(function(){
 
 		$('#subBtn1').click(function() {
-			var username = $('username').val();
-			var email = $('email').val();
+			var username = $('#username').val();
+			var email = $('#email').val(); 
 			var pass = $('#pass').val();
 			var conf = $('#conf').val();
 			if (username == '' || email == '' || pass == '' || conf == '') {
 				$("#alert").html("<div class='alert alert-danger alert-dismissible' role='alert'>" +
   						"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
   						"<strong>All fields are required!</strong></div>");
-			}
-			else if (pass !== conf) {
-				$("#alert").html("<div class='alert alert-danger alert-dismissible' role='alert'>" +
-  						"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
-  						"<strong>Password and conformation are not the same!</strong></div>");
 			}
 			else {
 				$.ajax({
@@ -23,22 +18,38 @@ $(document).ready(function(){
 					data: {
 						username: $('#username').val(),
 						email: $('#email').val(),
-						pass: $('#pass').val(),					
+						pass: $('#pass').val(),
+						conf: $('#conf').val()				
 				},
 				dataType: 'json',
 				success: function (response) {
-					if(response.status == 1){
+					if(response.status == 1) {
 						// if new user is registered
 						// redirect to...
 						$(window).attr('location','http://localhost/view/loggedIn.php');
+					} else if (response.status == "emailError") {
+						// alert if email is not in proper format
+						$("#emailAlert").html("<div class='alert alert-danger alert-dismissible' role='alert'>" + 
+						"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + 
+						"<strong>Ups! It looks like you typed invalid email address!</strong></div>");
+					} else if (response.status == "passError") {
+						// alert if conformation and password are not the same
+						$("#alert").html("<div class='alert alert-danger alert-dismissible' role='alert'>" + 
+						"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + 
+						"<strong>Ups! It looks like password and conformation are not the same!</strong></div>");
+					} else if (response.status == "weakPass") {
+						// alert if password is weak
+						$("#alert").html("<div class='alert alert-danger alert-dismissible' role='alert'>" + 
+						"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + 
+						"<strong>Your password must contain upper case letter, lower case letter and be at least 8 characters long!</strong></div>");
 					} else {
 						$("#takenName").html("<div class='alert alert-danger alert-dismissible' role='alert'>" + 
 						"<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + 
-						"<strong>Username is already taken!</strong></div>");
+						"<strong>Username is already taken!</strong></div>");						
 					}
 				},
 				error: function(xhr,ajaxOptions,thrownError) {
-					alert(thrownError); 
+					alert(thrownError);	 
 				}
 				
 				});
